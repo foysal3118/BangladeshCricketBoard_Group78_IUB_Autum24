ce6e6c4c42f51c0ee0e91a2cd655069d8ccaa51f
foysal3118 <foysaldurlov@gmail.com>
2024-11-28 04:29:50 +0600

    Faysal_28/11

---
+++ SimulatingOperationsOfBangladeshCricketBoard/src/main/java/bangladeshcricketboard/simulatingoperationsofbangladeshcricketboard/ForgetPasswordViewPageController.java
@@ -0,0 +1,13 @@
+package bangladeshcricketboard.simulatingoperationsofbangladeshcricketboard;
+
+import javafx.scene.control.Button;
+
+public class ForgetPasswordViewPageController
+{
+    @javafx.fxml.FXML
+    private Button testid;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+}
--- SimulatingOperationsOfBangladeshCricketBoard/src/main/java/bangladeshcricketboard/simulatingoperationsofbangladeshcricketboard/LoginViewPageController.java
+++ SimulatingOperationsOfBangladeshCricketBoard/src/main/java/bangladeshcricketboard/simulatingoperationsofbangladeshcricketboard/LoginViewPageController.java
@@ -1,7 +1,28 @@
 package bangladeshcricketboard.simulatingoperationsofbangladeshcricketboard;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class LoginViewPageController
 {
     @javafx.fxml.FXML
+    private TextField LogInUserIDTextField;
+    @javafx.fxml.FXML
+    private TextField LogInUserPasswordTestField;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void LogInNewSignUpButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void LogInForgotPasswordButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void LogInSignInButtonOnAction(ActionEvent actionEvent) {
+    }
+}
