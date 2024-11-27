1d4c987520a412f2ed2b98fa26bc854d851a8f11
foysal3118 <foysaldurlov@gmail.com>
2024-11-27 23:19:10 +0600

    Faysal_27/11

--- SimulatingOperationsOfBangladeshCricketBoard/src/main/java/bangladeshcricketboard/simulatingoperationsofbangladeshcricketboard/BcbMainApplicationClass.java
+++ SimulatingOperationsOfBangladeshCricketBoard/src/main/java/bangladeshcricketboard/simulatingoperationsofbangladeshcricketboard/BcbMainApplicationClass.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class BcbMainApplicationClass extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(BcbMainApplicationClass.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(BcbMainApplicationClass.class.getResource("LoginViewPage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("");
         stage.setScene(scene);
         stage.show();
     }
