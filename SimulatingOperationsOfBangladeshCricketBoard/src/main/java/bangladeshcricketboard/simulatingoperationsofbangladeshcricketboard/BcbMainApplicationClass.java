edbebd088f48a28fe5a78215ca76d2968c2fddb2
shihab-sani <mdsani633@gmail.com>
2024-12-06 23:48:32 +0600

    Logistic Manager Update

--- SimulatingOperationsOfBangladeshCricketBoard/src/main/java/bangladeshcricketboard/simulatingoperationsofbangladeshcricketboard/BcbMainApplicationClass.java
+++ SimulatingOperationsOfBangladeshCricketBoard/src/main/java/bangladeshcricketboard/simulatingoperationsofbangladeshcricketboard/BcbMainApplicationClass.java
@@ -1,16 +1,16 @@
 package bangladeshcricketboard.simulatingoperationsofbangladeshcricketboard;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class BcbMainApplicationClass extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(BcbMainApplicationClass.class.getResource("/user_dashboard_designs/logistic_manger_dashboard/LogisticManagerDashbordViewPage.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(BcbMainApplicationClass.class.getResource("/user_dashboard_designs/logistic_manger_dashboard/LogisticManagerFinanceDashbord.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("");
         stage.setScene(scene);
