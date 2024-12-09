7228c42dbd2d37965fe3fb1a118e5e6ad77127c8
shihab-sani <mdsani633@gmail.com>
2024-12-09 18:46:48 +0600

    shihab update application

--- SimulatingOperationsOfBangladeshCricketBoard/src/main/java/bangladeshcricketboard/simulatingoperationsofbangladeshcricketboard/BcbMainApplicationClass.java
+++ SimulatingOperationsOfBangladeshCricketBoard/src/main/java/bangladeshcricketboard/simulatingoperationsofbangladeshcricketboard/BcbMainApplicationClass.java
@@ -4,15 +4,36 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class BcbMainApplicationClass extends Application {
+
+    private double xOffSet = 0;
+    private double yOffSet = 0;
+
     @Override
     public void start(Stage stage) throws IOException {
+
+        stage.setResizable(false);
+        stage.initStyle(StageStyle.UNDECORATED);
+
         FXMLLoader fxmlLoader = new FXMLLoader(BcbMainApplicationClass.class.getResource("/user_dashboard_designs/logistic_manger_dashboard/LogisticManagerFinanceDashbord.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("");
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+
+        root.setOnMousePressed(event -> {
+            xOffSet = event.getSceneX();
+            yOffSet = event.getSceneY();
+        });
+
+        root.setOnMouseDragged(event -> {
+            stage.setX(event.getScreenX() - xOffSet);
+            stage.setY(event.getScreenY() - yOffSet);
+        });
+
         stage.setScene(scene);
         stage.show();
     }
